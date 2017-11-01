652af00255ce0457390392084ab8d9036b1052dd
mcwarman <mcwarman@gmail.com>
2017-11-01 15:39:55 +0000

    Build script tidying

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 set -e
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   echo "Initiating Docker Hub Build"
-  curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$TRAVIS_BRANCH\"}" -X POST "https://registry.hub.docker.com/u/mcwarman/interlok-hello-world/trigger/$DOCKER_HUB_TOKEN/"
+  curl -w "\n" -s -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$TRAVIS_BRANCH\"}" -X POST "https://registry.hub.docker.com/u/mcwarman/interlok-hello-world/trigger/$DOCKER_HUB_TOKEN/"
   echo "Initiated Docker Hub Build"
   echo "Deploying to Heroku"
   docker login --username=_ --password=$HEROKU_PASS registry.heroku.com;
-  docker tag $REPO:$TRAVIS_BUILD_NUMBER $HEROKU_REPO:latest;
+  docker tag $REPO:$TRAVIS_BUILD_NUMBER registry.heroku.com/$HEROKU_APP/web:latest;
   docker push $HEROKU_REPO:latest;
   docker logout registry.heroku.com;
   echo "Deployment to Heroku Complete"
