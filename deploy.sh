5f0b62a568a98082af8a53bd2da9b80805ee0f80
mcwarman <mcwarman@gmail.com>
2017-11-01 15:14:02 +0000

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -e
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   docker login --username=$DOCKER_USER --password=$DOCKER_PASS;
   docker tag $REPO:$TRAVIS_BUILD_NUMBER $REPO:latest;
-  docker push $REPO:$latest;
+  docker push $REPO:latest;
   docker logout;
   docker login --username=_ --password=$HEROKU_PASS registry.heroku.com;
   docker tag $REPO:$TRAVIS_BUILD_NUMBER $HEROKU_REPO:latest;
