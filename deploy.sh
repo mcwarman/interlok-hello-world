657695e21380a4604557589288d63e0b91b9eec8
mcwarman <mcwarman@gmail.com>
2018-07-18 18:17:32 +0100

    Heroku users stdin password

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
   docker logout;
   echo "Deployment to Docker Hub Complete"
   echo "Deploying to Heroku"
-  docker login --username=_ --password=$HEROKU_PASS registry.heroku.com;
+  echo "$HEROKU_PASS" | docker login -u "_" --password-stdin registry.heroku.com;
   docker tag $REPO:latest registry.heroku.com/$HEROKU_APP/web:latest;
   docker push registry.heroku.com/$HEROKU_APP/web:latest;
   docker logout registry.heroku.com;
