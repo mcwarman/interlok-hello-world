1897869d65da44e3456fc4c6fc80557b25f5238a
mcwarman <mcwarman@gmail.com>
2018-07-18 17:56:24 +0100

    Use gradle docker plugin

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
   echo "Initiated Docker Hub Build"
   echo "Deploying to Heroku"
   docker login --username=_ --password=$HEROKU_PASS registry.heroku.com;
-  docker tag $REPO:$TRAVIS_BUILD_NUMBER registry.heroku.com/$HEROKU_APP/web:latest;
+  docker tag $REPO:latest registry.heroku.com/$HEROKU_APP/web:latest;
   docker push registry.heroku.com/$HEROKU_APP/web:latest;
   docker logout registry.heroku.com;
   echo "Deployment to Heroku Complete"
